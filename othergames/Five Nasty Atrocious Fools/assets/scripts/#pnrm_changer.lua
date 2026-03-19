2e4c2acc0f1d4ca57f1af6fa5ff1a6f3ebc577d5
Ryan <sadw5000@gmail.com>
2026-03-19 17:18:42 -0400

    Removed unnecessary files.
    Bai Bai

--- othergames/Five Nasty Atrocious Fools/assets/scripts/#pnrm_changer.lua
+++
@@ -1,5 +0,0 @@
-events.OnEngineStart(function(e)
-    local PanoramaSettings = GameData.ext_data.skpanoramamodifier
-    MF_SetPanoramaValue(PanoramaSettings.Office)
-    MF_SetCameraPanoramaValue(PanoramaSettings.Camera)
-end)
